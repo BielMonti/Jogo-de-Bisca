8ccc241715476562a4b2b750876419ad1d3cebbd
Gabriel Montebeller <bielmontebeller@gmail.com>
2022-12-15 03:09:26 -0300

    mudar para 2 jogadores

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
-        Bisca bisca = new Bisca("Gabriel", "Guilherme", "Filipe", "Maja");
+        Bisca bisca = new Bisca("Gabriel", "Guilherme");
         bisca.jogar();
     }
 
